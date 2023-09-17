a14b7e611bab0d2505f16b20742c7bb9041d62b1
Daniil <105662394+MGS-Daniil@users.noreply.github.com>
2023-09-17 17:00:56 +0300

    Configurate

---
+++ gemfile
@@ -0,0 +1,2 @@
+﻿source "https://rubygems.org"
+gemspec
---
+++ js/gtag.js
@@ -0,0 +1,5 @@
+﻿window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-E31L9P86XR');
